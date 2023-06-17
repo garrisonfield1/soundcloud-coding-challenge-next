fb44337e9307f4f472e40d8e4e87c0c767bb32e5
garrisonfield1 <sean_mcintyre@hotmail.com>
2023-06-17 10:24:17 -0400

    working async but buggy promise loop

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+# SoundCloud Coding Challenge
+An Interface of Ice and Fire
+
+As an avid fan of the epic series A Song of Ice and Fire, I have a hard time keeping up with all of the characters who lose their heads. As such, I would like to have an application that can help me track which characters from the series are alive or dead. 
+
+
+Please provide an application that satisfies the following criteria:
+
+- Uses https://anapioficeandfire.com/ as a data source
+
+- Is built using your tech stack of choice
+
+- The Houses resource from the API (https://anapioficeandfire.com/api/houses) is paginated by default to 10 results, for these 10 houses please display a list of all of their Sworn Members grouped by house
+
+- For each Sworn Member display their full name and whether they are alive or dead
+
+- If a character is dead, please display the information provided by the API about their death
+
+- If a house has no Sworn Members, please display the message "This house has no sworn members"
+
+- You are welcome to use tools like ChatGPT if that is a part of your daily workflow but please be prepared to defend your choice to use those tools and provide alternate methods if requested on review
+
+
+Submissions will be accepted in the form of GitHub link, CodeSandbox link or zip file. Please provide instructions for how to run the application.
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
@@ -32,4 +57,3 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-# soundcloud-coding-challenge-next
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,94 @@
+export type House = {
+  url: string,
+  name: string,
+  region: string,
+  coatOfArms: string,
+  words: string,
+  titles: string[],
+  seats: string[],
+  currentLord: string,
+  heir: string,
+  overlord: string,
+  founded: string,
+  founder: string,
+  diedOut: string,
+  ancestralWeapons: string[],
+  cadetBranches: [],
+  swornMembers: []
+}
+
+type houseAndMembers = {
+  name:string, 
+  swornMembers: swornMembers[]
+}
+
+type swornMembers = {
+  name: string, 
+  deceased: string | boolean,
+  mannerOfDeath: string | null,
+  url: string
+}
+
+type HomeProps = {
+  houseAndMembers: houseAndMembers[]
+};
+
+
+
+
+const Home = ({houseAndMembers}: HomeProps) => {
+
+  return (
+// playing wiht tailwind
+// <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+//   {/* <img src="/img/beams.jpg" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" /> */}
+//   <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,black,rgba(0, 0, 0, 0.5))]"></div>
+//   {/* <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"></div> */}
+//   <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-auto sm:rounded-lg sm:px-10">
+//     <div class="mx-auto max-w-md">
+//       {/* <img src="/img/logo.svg" class="h-6" alt="Tailwind Play" /> */}
+//       <div class="divide-y divide-gray-300/50">
+//         <table class="border-collapse border border-slate-500 ...">
+//           <thead>
+//             <tr>
+//               <th class="border border-slate-600 ...">State</th>
+//               <th class="border border-slate-600 ...">City</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr>
+//               <td class="border border-slate-700 ...">Indiana</td>
+//               <td class="border border-slate-700 ...">Indianapolis</td>
+//             </tr>
+//             <tr>
+//               <td class="border border-slate-700 ...">Ohio</td>
+//               <td class="border border-slate-700 ...">Columbus</td>
+//             </tr>
+//             <tr>
+//               <td class="border border-slate-700 ...">Michigan</td>
+//               <td class="border border-slate-700 ...">Detroit</td>
+//             </tr>
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+    <div className="bg-blend-multiply bg-blend-overlay">
+      {houseAndMembers.map(house => {
+        return (
+          <>---------
+            <div key={house.name}>{house.name}</div>
+            {
+              house.swornMembers ? house.swornMembers.map( member => <div key={member.name}>{member.name}</div> )
+              : 
+              <div>This house has no sworn members</div>
+            }
+          </>
+        )
+      })}
+    </div>
+  )
+}
+export default Home;
