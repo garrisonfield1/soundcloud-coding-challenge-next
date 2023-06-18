65b248e720b63725b8ad285a2ee1252d6ba5f095
garrisonfield1 <sean_mcintyre@hotmail.com>
2023-06-18 12:52:07 -0400

    style and bg added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Cinzel } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Cinzel({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'A Song of Ice and Fire',
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet"></link>
+      </head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -37,26 +37,20 @@ type HomeProps = {
 const Home = ({houseAndMembers}: HomeProps) => {
 
   return (    
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <div className="absolute inset-0 bg-[url(../assets/img/bg.jpg)] bg-center [mask-image:linear-gradient(180deg,black,rgba(0, 0, 0, 0.5))]"></div>
-      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 mx-auto sm:rounded-lg sm:px-10 w-screen">
-        <div className="mx-auto ">
-          <div className="divide-y divide-gray-300/50">
+    <div className="relative p-6 flex min-h-screen flex-col overflow-hidden inset-0 bg-[url(../assets/img/dragon.jpg)] bg-blend-overlay bg-cover bg-slate-700 h-screen ">
+        <div className="overflow-y-scroll">
+          <div className="overflow-y-scroll ">
           {houseAndMembers.map(house => {
               return (
-                <div className="grid grid-rows-3 grid-flow-col " key={house.name}>
-                  <div className="row-span-1 ..." key={house.name}>{house.name}</div>
+                <div key={house.name} className="rounded-lg border-2 border-white p-6 m-6">
+                  <h1 key={house.name}>{house.name}</h1>
                   {
-                    house.swornMembers ? house.swornMembers.map( member =>  (
-                    <>
-                      <div className="col-span-3..." key={member.name} >{member.name}</div>
-                      <div className="col-span-1 ..." >{member.died}</div>
-                      <div className="col-span-1 ..." key={member.url} ><a href={member.url} target="_blank" >url</a></div>
-                      {/* <td className="border border-slate-700 ..." key={member.died} >{member.died ? 'Deceased' : 'Alive'}</td> */}
-                      {/* {member.died ? <td className="border border-slate-700 ..." key={member.died} >died {member.died}</td> : null  } */}
-                    </>
+                    house.swornMembers ? house.swornMembers.map( (member, i) =>  (
+                    <div key={i} className="grid grid-cols-2 ">
+                      <div key={i} >{member.name}</div>
+                      { member.died ? <div >Died in {member.died}</div> : <div></div>}
+                    </div>
                     ))
-                    // <td className="border border-slate-700 ...">Indianapolis</td>
                     : 
                     <div className="col-span-1 ..." >This house has no sworn members</div>
                   }
@@ -65,7 +59,6 @@ const Home = ({houseAndMembers}: HomeProps) => {
             })}
           </div>
         </div>
-      </div>
     </div>    
   )
 }
